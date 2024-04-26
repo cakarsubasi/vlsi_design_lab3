79bec51c05feada9201d84efed1eecfd73af506a
Cem Akarsubasi <cemakarsubasi@gmail.com>
2024-04-26 12:09:02 +0200

    Simplify top level design

--- fp32mul_pipe.vhd
+++ fp32mul_pipe.vhd
@@ -16,16 +16,14 @@ architecture SCHEMATIC of fp32mul_pipe is
    signal       MX : std_logic_vector(23 downto 0);
    signal       MY : std_logic_vector(23 downto 0);
    signal       MZ : std_logic_vector(23 downto 0);
-   signal       MZ1 : std_logic_vector(23 downto 0); -- pipelined after significand
    signal       EXP : std_logic_vector(7 downto 0);
+   signal       EXP_pipe : std_logic_vector(7 downto 0);
    signal       EXP1 : std_logic_vector(7 downto 0);
-   signal       EXP_1 : std_logic_vector(7 downto 0); -- pipelined after expincr
-   signal       EXP1_1 : std_logic_vector(7 downto 0); -- pipelined after expincr
+   signal       EXP1_pipe : std_logic_vector(7 downto 0);
    signal       EZ : std_logic_vector(7 downto 0);
    signal       OVF : std_logic;
-   signal       OVF1: std_logic; -- pipelined after significand
    signal       SZ : std_logic;
-   signal       SZ1: std_logic; -- pipelined after outsign
+   signal       SZ_pipe : std_logic;
 
 
    component reg32b
@@ -33,6 +31,14 @@ architecture SCHEMATIC of fp32mul_pipe is
 		CLOCK : In std_logic;
 		RESET : In std_logic;
 		Z : Out std_logic_vector (31 downto 0) );
+   end component;
+
+   component regnb
+   Generic (n: integer);
+	Port (	A : In std_logic_vector (n-1 downto 0);
+		CLOCK : In std_logic;
+		RESET : In std_logic;
+		Z : Out std_logic_vector (n-1 downto 0) );
    end component;
 
    component out_sign
@@ -65,7 +71,7 @@ architecture SCHEMATIC of fp32mul_pipe is
                 Z : Out std_logic_vector (23 downto 0);
                 CLOCK: In std_logic;
                 RESET: In std_logic );
-   end component; -- pipelined by 1
+   end component;
 
    component gl_mux21 
         GENERIC(n : integer);
@@ -87,52 +93,41 @@ begin
    EXP_ADD : expadd
 	Port Map ( A1(7 downto 0)=>BX(30 downto 23), 
 		   A2(7 downto 0)=>BY(30 downto 23),
-		   S(7 downto 0)=>EXP(7 downto 0) ); 
+		   S(7 downto 0)=>EXP_pipe(7 downto 0) ); 
    EXP_INCR : expincrement
-	Port Map ( EXP(7 downto 0)=>EXP(7 downto 0), 
+	Port Map ( EXP(7 downto 0)=>EXP_pipe(7 downto 0), 
 		   EZ(7 downto 0)=>EXP1(7 downto 0) );
-
-   REGPIPE1 : reg32b
-   Port Map ( 
-      A(7 downto 0)=>EXP(7 downto 0), 
-      A(15 downto 8)=>EXP1(7 downto 0),
-      A(16)=>OVF,
-      A(17)=>SZ,
-      A(31 downto 18) => "--------------",
-      CLOCK=>CLOCK,
-      RESET=>RESET, 
-      Z(7 downto 0)=>EXP_1(7 downto 0),
-      Z(15 downto 8)=>EXP1_1(7 downto 0),
-      Z(16)=>OVF1,
-      Z(17)=>SZ1
-   );
-
-   REGPIPE2 : reg32b
-   Port Map (
-      A(22 downto 0)=> MZ(22 downto 0),
-      A(31 downto 23)=> "---------",
-      CLOCK=>CLOCK,
-      RESET=>RESET, 
-      Z(22 downto 0)=> MZ1(22 downto 0)
-   );
 
    -- must include implicit 1
    MX(23)<='1'; MX(22 downto 0) <= BX(22 downto 0); 
    MY(23)<='1'; MY(22 downto 0) <= BY(22 downto 0); 
 
+   REGPIPE : regnb
+   Generic Map (n=>17)
+   Port Map (
+      A(7 downto 0) => EXP_pipe,
+      A(15 downto 8) => EXP1_pipe,
+      A(16) => SZ_pipe,
+      CLOCK=>CLOCK,
+      RESET=>RESET,
+      Z(7 downto 0) => EXP,
+      Z(15 downto 8) => EXP1,
+      Z(16) => SZ
+   );
+
    SIGNIFICAND : significand_compute
 	Port Map ( MX=>MX, MY=>MY, OVF=>OVF, Z=>MZ, CLOCK=>CLOCK, RESET=>RESET );
 
    EXP_MUX : gl_mux21 Generic Map(n=>7)
-        Port Map ( A0 => EXP_1, A1  => EXP1_1, SEL => OVF1, Z => EZ );
+        Port Map ( A0 => EXP, A1  => EXP1, SEL => OVF, Z => EZ );
 
    SIGN_COMP : out_sign
-	Port Map (  SX=>BX(31), SY=>BY(31), SZ=>SZ );
+	Port Map (  SX=>BX(31), SY=>BY(31), SZ=>SZ_pipe );
 
    REGZ : reg32b
-	Port Map ( A(31)=>SZ1,
+	Port Map ( A(31)=>SZ,
 		   A(30 downto 23)=>EZ(7 downto 0), 
-		   A(22 downto 0)=>MZ1(22 downto 0), 
+		   A(22 downto 0)=>MZ(22 downto 0), 
 		   CLOCK=>CLOCK, RESET=>RESET, 
          Z(31 downto 0)=>Z(31 downto 0) );
 
